518f60b918b6006c6185e410c0e538bc8268b6d4
sean lewis <seanmartinlewis@gmail.com>
2016-10-20 11:37:02 -0500

    append li

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -86,11 +86,23 @@ function loadGrowls() {
   $.ajax({
   url: 'http://localhost:3000/'
     })
-  .done(function (response) {
-    console.log(response);
+  .done(function (data) {
+    console.log(data);
+    data.forEach(function (datum) {
+      loadGrowl(datum)
+    })
   })
   .fail(function (jqXHR, textStatus, errorThrown) {
     console.log(errorThrown);
   });
+}
 
+function loadGrowl(data) {
+  console.log(data);
+  var li = $('<li />')
+  li.text(data.content)
+
+  $('#user-growls').append(li)
+  $('#spostGrowl').val('')
+  $
 }
